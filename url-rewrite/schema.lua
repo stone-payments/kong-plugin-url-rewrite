8c40dd0891eabddfe50028db3b71998237cd26f9
Jessica Paz <jessica.paz@stone.com.br>
2020-03-05 15:24:30 -0300

    Add query_string schema field

--- url-rewrite/schema.lua
+++ url-rewrite/schema.lua
@@ -1,6 +1,7 @@
 return {
   no_consumer = true,
   fields = {
-    url = {required = true, type = "string"}
+    url = {required = true, type = "string"},
+    query_string = {required = false, type = "table"}
   }
 }
